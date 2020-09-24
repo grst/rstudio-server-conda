9ad6c98e335ec9bd75818b1dbf94f82185df3896
Gregor Sturm <mail@gregor-sturm.de>
2020-09-24 09:54:09 +0200

    deactivate conda env before activating

--- rsession.sh
+++ rsession.sh
@@ -15,9 +15,14 @@ CONDA_ENV=`cat /tmp/rstudio-server/${USER}_current_env`
 echo "## CONDA ENV is >>>"
 echo ${CONDA_ENV}
 
+conda deactivate
+conda deactivate
+conda deactivate
 conda activate ${CONDA_ENV}
 
 export RETICULATE_PYTHON=$CONDA_PREFIX/bin/python
 export TERM=linux
+
+source set_blas_cores 1
 
 /usr/lib/rstudio-server/bin/rsession $@
