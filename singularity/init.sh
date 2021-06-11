94eaf7cc5ed8aeb939317d0acfad1859953d5ac4
Gregor Sturm <mail@gregor-sturm.de>
2021-06-11 11:42:25 +0200

    Update singularity method

---
+++ singularity/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /opt/conda/etc/profile.d/conda.sh && \
+  conda activate $CONDA_PREFIX && \
+  rserver \
+    --www-address=127.0.0.1 \
+    --www-port=$PORT \
+    --rsession-which-r=$RSTUDIO_WHICH_R \
+    --rsession-ld-library-path=$CONDA_PREFIX/lib \
+    `# optional: old behaviour of R sessions` \
+    --auth-timeout-minutes=0 --auth-stay-signed-in-days=30  \
+    `# activate password authentication` \
+    --auth-none=0  --auth-pam-helper-path=pam-helper
+
