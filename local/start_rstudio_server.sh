462eb6059035cbefec2cb32f022a32c1e0d4a8f8
Suthep Pomjaksilp <git@laz0r.de>
2021-11-16 12:35:28 +0100

    Add user specific BASE_PATH
    BASE_PATH defines a user specific base path in /tmp/ in start_rstudio_server.sh.
    This resolves an issue in a multi-user scenario where starting a second
    session will lead to a permission denied on /tmp/rstudio-server-utils
    because the directory is 755.

--- local/start_rstudio_server.sh
+++ local/start_rstudio_server.sh
@@ -7,15 +7,17 @@
 
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 USER=$(whoami)
+# set a user specific base path
+BASE_PATH=/tmp/rstudio-server-${USER}
 # set a user-specific secure cookie key
-COOKIE_KEY_PATH=/tmp/rstudio-server/${USER}_secure-cookie-key
+COOKIE_KEY_PATH=${BASE_PATH}/${USER}_secure-cookie-key
 rm -f $COOKIE_KEY_PATH
 mkdir -p $(dirname $COOKIE_KEY_PATH)
 
 # Rserver >= version 1.3 requires the --auth-revocation-list-dir parameter
 if [ $(sed -n '/^1.3./p;q' /usr/lib/rstudio-server/VERSION) ] ;
 then
-  REVOCATION_LIST_DIR=/tmp/rstudio-server/${USER}_revocation-list-dir
+  REVOCATION_LIST_DIR=${BASE_PATH}/${USER}_revocation-list-dir
   mkdir -p $REVOCATION_LIST_DIR
   REVOCATION_LIST_PAR="--auth-revocation-list-dir=$REVOCATION_LIST_DIR"
 else
@@ -26,7 +28,7 @@ python -c 'import uuid; print(uuid.uuid4())' > $COOKIE_KEY_PATH
 chmod 600 $COOKIE_KEY_PATH
 
 # store the currently activated conda environment in a file to be read by rsession.sh
-CONDA_ENV_PATH=/tmp/rstudio-server/${USER}_current_env
+CONDA_ENV_PATH=${BASE_PATH}/${USER}_current_env
 rm -f $CONDA_ENV_PATH
 echo "## Current env is >>"
 echo $CONDA_PREFIX
