62cd640677845c7e5b5f282bf448a58475966633
Fabian Kern <>
2023-02-01 11:11:38 +0100

    Simplified previous bug fix and fixed more permission errors in multi-user environments caused by recent rstudio changes.

--- local/start_rstudio_server.sh
+++ local/start_rstudio_server.sh
@@ -35,11 +35,13 @@ echo $CONDA_PREFIX > $CONDA_ENV_PATH
 export RETICULATE_PYTHON=$CONDA_PREFIX/bin/python
 
 # store the current path in database config
-sed -i "s,directory=.*,directory=$CWD," $CWD/database.conf
+sed -i "s,directory=.*,directory=\/tmp\/rstudio-server\/${USER}_database," $CWD/database.conf
 
 /usr/lib/rstudio-server/bin/rserver --server-daemonize=0 \
   --www-port=$1 \
   --secure-cookie-key-file=$COOKIE_KEY_PATH \
+  --server-pid-file="$CWD/rstudio-server.pid" \
+  --server-data-dir="$CWD/rstudio-server" \
   --rsession-which-r=$(which R) \
   --rsession-ld-library-path=$CONDA_PREFIX/lib \
   --rsession-path="$CWD/rsession.sh" \
